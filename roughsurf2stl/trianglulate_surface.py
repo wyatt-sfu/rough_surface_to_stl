e72cd5d0a78492ab1f1022cd7bbcbc474eb1f125
Wyatt Gronnemose <wgronnemose@airadar.com>
2025-05-05 14:28:08 -0700

    Added computation of surface normal vector

--- roughsurf2stl/trianglulate_surface.py
+++ roughsurf2stl/trianglulate_surface.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.spatial
+import tqdm
 
 
 def triangulate_surface(nx, ny, dx, surf_height) -> tuple[np.ndarray, np.ndarray]:
@@ -21,7 +22,9 @@ def triangulate_surface(nx, ny, dx, surf_height) -> tuple[np.ndarray, np.ndarray
     surf_xy_indices = np.reshape(surf_xy_indices, [nx * ny, 2])
 
     # Triangulate the surface using Delaunay tesselation
+    print("Triangulating the XY plane...")
     xy_triangles = scipy.spatial.Delaunay(surf_xy_indices)
+    print("... Complete")
     vertex_indices = xy_triangles.simplices
     triangles = triangle_coord_lookup(
         vertex_indices, surf_xy_indices, x_points, y_points, surf_height
@@ -37,7 +40,7 @@ def triangle_coord_lookup(
     n_triangles = vertex_indices.shape[0]
     triangles = np.zeros((n_triangles, 3, 3))
 
-    for i in range(n_triangles):
+    for i in tqdm.trange(n_triangles, desc="Triangle vertices:"):
         _tri = np.zeros((3, 3))  # Each row is the x,y,z coordinates of a vertex
         for j in range(3):
             # Loop over each vertex in the triangle
@@ -54,4 +57,18 @@ def triangle_coord_lookup(
 
 def triangle_normals(triangles):
     """Compute a normal unit vector to each triangle."""
-    return np.zeros((triangles.shape[0], 3))  # TODO: Actually compute this
+    n_triangles = triangles.shape[0]
+    normals = np.zeros((n_triangles, 3))
+
+    for i in tqdm.trange(n_triangles, desc="Triangle normals:"):
+        v1 = triangles[i, 0, :]
+        v2 = triangles[i, 1, :]
+        v3 = triangles[i, 2, :]
+        n_hat = np.cross((v2 - v1), (v3 - v1))
+        if n_hat[2] < 0.0:
+            # The vertices were not arranged counter-clockwise.
+            n_hat = np.cross((v3 - v1), (v2 - v1))
+        n_hat = n_hat / np.linalg.norm(n_hat)
+        normals[i, :] = n_hat
+
+    return normals
